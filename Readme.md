95aed94061cbfb6e58f53a1fea2bfa55938360e5
Giovanni <GiovanniGiampaolo@users.noreply.github.com>
2020-12-22 11:52:54 +0100

    ci: inizialized and finalized

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## A simple CRA for testing the i18next lib for translation
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {useTranslation} from 'react-i18next'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {t, i18n} = useTranslation()
+
+    function handleClick(lan: string) {
+        i18n.changeLanguage(lan)
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <nav style={{width: '100%', padding: '1rem 0'}}>
+                    {LANGUAGES.map((lan: {label: string, val: string})=>
+                        <button className="t-button" key={lan.val} onClick={() => handleClick(lan.val)}>{lan.label}</button>)}
+                </nav>
+                <h4>{t('welcome')}</h4>
+                <p>{t('description.part1')}</p>
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+const LANGUAGES = [
+    {label: 'Italiano', val: 'it'},
+    {label: 'English', val: 'en'},
+    {label: 'Français', val: 'fr'},
+    {label: 'Deutsche', val: 'de'},
+    {label: 'Español', val: 'es'},
+    {label: '中文', val: 'chi'},
+]
